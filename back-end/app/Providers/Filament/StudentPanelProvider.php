ac14b807e63c87a396549e93f0288eeaf1ca215d
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2024-07-26 15:22:34 +0000

    Update StudentPanelProvider.php

--- back-end/app/Providers/Filament/StudentPanelProvider.php
+++ back-end/app/Providers/Filament/StudentPanelProvider.php
@@ -25,6 +25,7 @@ class StudentPanelProvider extends PanelProvider
         return $panel
             ->id('student')
             ->path('student')
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
