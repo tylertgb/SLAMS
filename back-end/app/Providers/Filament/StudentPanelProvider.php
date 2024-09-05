36f90be440cb0e516bec99155bd8a1a6e939b9fe
Tyler Gobka Bright <tgbright88@gmail.com>
2024-09-05 14:04:31 +0000

    updates

--- back-end/app/Providers/Filament/StudentPanelProvider.php
+++ back-end/app/Providers/Filament/StudentPanelProvider.php
@@ -43,7 +43,7 @@ class StudentPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Student/Widgets'), for: 'App\\Filament\\Student\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
